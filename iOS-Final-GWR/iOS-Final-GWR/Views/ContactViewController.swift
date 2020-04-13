31d195a87be06e497e46ebb0e57fa7976fd694a9
Dawson Neves de Araujo <dawsonaraujo13@gmail.com>
2020-04-13 16:11:55 -0400

    Added Contact Page Functions

--- iOS-Final-GWR/iOS-Final-GWR/Views/ContactViewController.swift
+++ iOS-Final-GWR/iOS-Final-GWR/Views/ContactViewController.swift
@@ -12,7 +12,31 @@ class ContactViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    @IBOutlet weak var phoneButton: UIButton!
+    @IBOutlet weak var emailButton: UIButton!
+    @IBOutlet weak var addressButton: UIButton!
+    
+    @IBAction func clickedPhone(_ sender: Any) {
+        let url: NSURL = URL(string: "TEL://7863133115")! as NSURL
+        UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func clickedEmail(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "mailto:manager@godswayradio.com")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func clickedAddress(_ sender: Any) {
+
+        let address = "11975,SW+2+Street,Miami,FL,33184,USA"
+        
+        if let url = URL(string:"http://maps.apple.com/?address=\(address)") {
+            UIApplication.shared.open(url)
+        }
+
+    }
+    
 }
 
